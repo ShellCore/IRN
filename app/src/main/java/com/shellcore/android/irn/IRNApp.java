3516edcbe77f09cc2e542e49f786f6e2660238d3
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-08-01 15:29:29 -0500

    Personalización de la vista principal

---
+++ app/src/main/java/com/shellcore/android/irn/IRNApp.java
@@ -0,0 +1,15 @@
+package com.shellcore.android.irn;
+
+import android.app.Application;
+
+/**
+ * Created by Cesar on 01/08/2017.
+ */
+
+public class IRNApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
--- app/src/main/java/com/shellcore/android/irn/MainActivity.java
+++ app/src/main/java/com/shellcore/android/irn/MainActivity.java
@@ -1,13 +1,55 @@
 package com.shellcore.android.irn;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.img_status)
+    ImageView imgStatus;
+    @BindView(R.id.txt_status)
+    TextView txtStatus;
+    @BindView(R.id.progress_status)
+    ProgressBar progressStatus;
+    @BindView(R.id.content)
+    CoordinatorLayout content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_update:
+                break;
+            case R.id.action_show_tables:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
